c1333fffd54fba196016acd12e48ab9ef2d1c430
liuhaifeng <liuhaifeng@deepexi.com>
2020-12-30 18:14:41 +0800

    add include file

--- prd01-tc-lb/bpf_lxc.c
+++ prd01-tc-lb/bpf_lxc.c
@@ -7,10 +7,63 @@
 #include <linux/icmpv6.h>
 #define EVENT_SOURCE LXC_ID
 
+#include "lib/tailcall.h"
+#include "lib/common.h"
+#include "lib/config.h"
+#include "lib/maps.h"
+#include "lib/arp.h"
+#include "lib/edt.h"
+#include "lib/ipv6.h"
+#include "lib/ipv4.h"
+#include "lib/icmp6.h"
+#include "lib/eth.h"
+#include "lib/dbg.h"
+#include "lib/l3.h"
+#include "lib/lxc.h"
+#include "lib/nat46.h"
+#include "lib/identity.h"
+#include "lib/policy.h"
+#include "lib/lb.h"
+#include "lib/drop.h"
+#include "lib/dbg.h"
+#include "lib/trace.h"
+#include "lib/csum.h"
+#include "lib/encap.h"
+#include "lib/eps.h"
+#include "lib/nat.h"
+#include "lib/fib.h"
+#include "lib/nodeport.h"
+#include "lib/policy_log.h"
+
+static __always_inline int handle_ipv4_from_lxc(struct __ctx_buff *ctx,
+						__u32 *dstID)
+{
+    return CTX_ACT_OK;
+}
+
 __section("from-container")
 int handle_xgress(struct __ctx_buff *ctx)
 {
-    return CTX_ACT_OK;
+    __u16 proto;
+    __u32 dstID = 0;
+    int ret = CTX_ACT_OK;
+    bpf_clear_meta(ctx);
+
+    if(!validate_ethertype(ctx,&proto)){
+        ret = DROP_UNSUPPORTED_L2;
+        goto out;
+    }
+    switch(proto){
+	case bpf_htons(ETH_P_IP):
+		ret = handle_ipv4_from_lxc(ctx, &dstID);
+		break;
+	case bpf_htons(ETH_P_IPV6):
+	    // do something
+	default:
+		ret = DROP_UNKNOWN_L3;
+    }
+out:
+    return ret;
 }
 
 __section("to-container")
--- prd01-tc-lb/env.sh
+++ prd01-tc-lb/env.sh
@@ -10,4 +10,5 @@ ip netns exec bpf_ns1 ip addr add 172.18.1.10/24 dev bpf_veth1
 ip addr add 172.18.1.11/24 dev bpf_veth0
 ip netns exec bpf_ns1 ip link set bpf_veth1 up
 ip link set bpf_veth0 up
+ip netns exec bpf_ns1 ip route add 0.0.0.0/0 via 172.18.1.11 dev bpf_veth1
 ping -c 5 172.18.1.10
